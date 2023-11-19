5ceecf649b0346c72aadf61fcf60877f0224dd53
proxima19 <44165934+proxima19a@users.noreply.github.com>
2023-11-19 22:30:26 +0300

    Add lab3

---
+++ js-info.js
@@ -0,0 +1,5 @@
+alert('Язык программирования JavaScript разработал Брендан Эйх (Brandan Eich)');
+alert('JavaScript был написан за 10 дней.');
+alert('JavaScript появился в 1995 году.');
+alert('JavaScript - это не Java');
+alert('Спецификация языка JavaScript называется ECMAScript.');
